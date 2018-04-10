3dcc087cb8452c167ea647e533c537a284207f81
zhuliang <zhuliang@qiniu.com>
2018-04-10 19:19:15 +0800

    add VideoReader declaration

--- pytorch/nvvl/__init__.py
+++ pytorch/nvvl/__init__.py
@@ -1,5 +1,6 @@
 from .dataset import VideoDataset,ProcessDesc
 from .loader import VideoLoader
+from .video_reader import VideoReader
 
 def video_size_from_file(filename):
     return lib.nvvl_video_size_from_file(str.encode(filename))
